5404abffd18fb985613b8a410a05c2fb7120e81d
timomegner-ui <timomegner@googlemail.com>
2026-04-21 18:06:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,6 @@ def home():
     return "License Server läuft!"
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
