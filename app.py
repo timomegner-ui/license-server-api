1eaac1bf4cfd01f7f3d35a656eb02a7a1a56adaf
timomegner-ui <timomegner@googlemail.com>
2026-04-21 18:14:17 +0200

    Format PORT environment variable retrieval
    Add a space after the default port value for consistency.

--- app.py
+++ app.py
@@ -27,5 +27,6 @@ def home():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT",10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port))
+ 
